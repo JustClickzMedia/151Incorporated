80885498392f5caea2de84a21f892510282c0432
Kenneth Elliott <kenneth.elliott07@gmail.com>
2024-03-21 17:12:14 -0400

    upgrade

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { AppBrowserModule } from '.././src/app/app.browser.module';
-import { enableProdMode } from '@angular/core'
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
